71ed2e770b6bef6a4b9a3d46461772f6f0273cf7
Lily Finley <TechnoFairyGirl@users.noreply.github.com>
2022-04-01 23:46:44 -0500

    Initial code commit.

---
+++ HP_3488A.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrologixGPIB;
+
+namespace HpSwitchControlApi
+{
+	class HP_3488A : IDisposable
+	{
+		public enum DigitalPort : int
+		{
+			LowByte = 0,
+			HighByte = 1,
+		}
+
+		public string Host { get => gpib.Host; }
+		public int Address { get => gpib.Address; }
+
+		readonly GPIB gpib;
+
+		public HP_3488A(string host, int address, bool configureAdapter = true)
+		{
+			gpib = new GPIB(host, address, configureAdapter: configureAdapter);
+		}
+
+		public void Dispose() =>
+			gpib.Dispose();
+
+		static int ChannelAddress(int slot, int channel) =>
+			(100 * slot) + channel;
+
+		public void SetState(int slot, int channel, bool state) =>
+			gpib.Query($"{(state ? "OPEN" : "CLOSE")} {ChannelAddress(slot, channel)}");
+
+		public bool GetState(int slot, int channel) =>
+			gpib.Query($"VIEW {ChannelAddress(slot, channel)}")
+				.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+				[0] == "OPEN";
+
+		public void DigitalWrite(int slot, DigitalPort port, byte val) =>
+			gpib.Query($"DWRITE {ChannelAddress(slot, (int)port)},{val}");
+
+		public byte DigitalRead(int slot, DigitalPort port) =>
+			byte.Parse(gpib.Query($"DREAD {ChannelAddress(slot, (int)port)}"));
+
+		public void Local() =>
+			gpib.Local();
+
+		public void DisplayString(string str) =>
+			gpib.Query($"DISP {str}");
+
+		public void DisplayOn() =>
+			gpib.Query($"DON");
+
+		public void DisplayOff() =>
+			gpib.Query($"DOFF");
+
+		public void Reset() =>
+			gpib.Query($"RESET");
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,128 @@
+﻿using Newtonsoft.Json;
+using SimpleHttp;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HpSwitchControlApi
+{
+	sealed class Config
+	{
+		public string GpibHost { get; set; }
+		public int GpibAddress { get; set; }
+		public int HttpPort { get; set; }
+		public string HttpToken { get; set; }
+	}
+
+	static class Program
+	{
+		static Config config;
+
+		static T DoCommand<T>(Func<HP_3488A, T> callback, bool configureAdapter = false)
+		{
+			using (var device = new HP_3488A(config.GpibHost, config.GpibAddress, configureAdapter))
+				return callback(device);
+		}
+
+		static void DoCommand(Action<HP_3488A> callback, bool configureAdapter = false)
+		{
+			using (var device = new HP_3488A(config.GpibHost, config.GpibAddress, configureAdapter))
+				callback(device);
+		}
+
+		static void Main(string[] args)
+		{
+			var exePath = System.Reflection.Assembly.GetEntryAssembly().Location;
+			var configPath = args.Length >= 1 ? args[0] : Path.Combine(Path.GetDirectoryName(exePath), "config.json");
+			config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(configPath));
+
+			var lockObj = new object();
+
+			var server = new HttpServer(config.HttpPort);
+
+			server.Log($"Using host '{config.GpibHost}', GPIB address {config.GpibAddress}.");
+
+			DoCommand(dev => dev.Reset(), configureAdapter: true);
+
+			server.AddRoute("GET", @"/slot/(\d+)/channel/(\d+)", (urlArgs, request, response) =>
+			{
+				var state = false;
+
+				lock (lockObj)
+					state = DoCommand(dev => dev.GetState(int.Parse(urlArgs[0]), int.Parse(urlArgs[1])));
+
+				response.WriteBodyJson(state);
+			});
+
+			server.AddRoute("POST", @"/slot/(\d+)/channel/(\d+)", (urlArgs, request, response) =>
+			{
+				var state = request.ReadBodyJson<bool>();
+
+				lock (lockObj)
+					DoCommand(dev => dev.SetState(int.Parse(urlArgs[0]), int.Parse(urlArgs[1]), state));
+			});
+
+			server.AddRoute("GET", @"/slot/(\d+)/digital-port/([^/]+)", (urlArgs, request, response) =>
+			{
+				var port =
+					urlArgs[1] == "low-byte" ? HP_3488A.DigitalPort.LowByte :
+					urlArgs[1] == "high-byte" ? HP_3488A.DigitalPort.HighByte :
+					throw new ArgumentException();
+
+				var value = 0;
+
+				lock (lockObj)
+					value = DoCommand(dev => dev.DigitalRead(int.Parse(urlArgs[0]), port));
+
+				response.WriteBodyJson(value);
+			});
+
+			server.AddRoute("POST", @"/slot/(\d+)/digital-port/([^/]+)", (urlArgs, request, response) =>
+			{
+				var port =
+					urlArgs[1] == "low-byte" ? HP_3488A.DigitalPort.LowByte :
+					urlArgs[1] == "high-byte" ? HP_3488A.DigitalPort.HighByte :
+					throw new ArgumentException();
+
+				var value = request.ReadBodyJson<byte>();
+
+				lock (lockObj)
+					DoCommand(dev => dev.DigitalWrite(int.Parse(urlArgs[0]), port, value));
+			});
+
+			server.AddExactRoute("POST", @"/reset", (request, response) =>
+			{
+				lock (lockObj)
+					DoCommand(dev => dev.Reset());
+			});
+
+			server.AddExactRoute("POST", @"/local", (request, response) =>
+			{
+				lock (lockObj)
+					DoCommand(dev => dev.Local());
+			});
+
+			server.AddExactRoute("POST", @"/display/text", (request, response) =>
+			{
+				var text = request.ReadBodyJson<string>();
+
+				lock (lockObj)
+					DoCommand(dev => dev.DisplayString(text));
+			});
+
+			server.AddExactRoute("POST", @"/display/clear", (request, response) =>
+			{
+				lock (lockObj)
+					DoCommand(dev => dev.DisplayOn());
+			});
+
+			server.Start();
+
+			Task.Delay(-1).Wait();
+		}
+	}
+}
